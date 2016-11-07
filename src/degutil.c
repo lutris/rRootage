0a119aca26888b139e9d06c64ed3c5bac78b2e00
Mathieu Comandon <strycore@gmail.com>
2016-11-07 00:03:21 -0800

    More formatting

--- src/degutil.c
+++ src/degutil.c
@@ -17,15 +17,17 @@ int sctbl[SC_TABLE_SIZE+SC_TABLE_SIZE/4];
 
 void initDegutil() {
   int i, d = 0;
-  double od = 6.28/DIV;
-  for ( i=0 ; i<TAN_TABLE_SIZE ; i++ ) {
-    while ( (int)(sin(d*od)/cos(d*od)*TAN_TABLE_SIZE)<i ) d++;
+  double od = 6.28 / DIV;
+  for (i=0; i < TAN_TABLE_SIZE; i++) {
+    while ((int)(sin(d*od)/cos(d*od)*TAN_TABLE_SIZE)<i) {
+        d++;
+    }
     tantbl[i] = d;
   }
   tantbl[TAN_TABLE_SIZE] = tantbl[TAN_TABLE_SIZE+1] = 128;
 
-  for ( i=0 ; i<SC_TABLE_SIZE+SC_TABLE_SIZE/4 ; i++ ) {
-    sctbl[i] = (int)(sin(i*(6.28/SC_TABLE_SIZE))*256);
+  for (i=0 ; i < SC_TABLE_SIZE + SC_TABLE_SIZE / 4 ; i++) {
+    sctbl[i] = (int)(sin(i * (6.28 / SC_TABLE_SIZE)) * 256);
   }
 }
 
@@ -37,50 +39,58 @@ int getDeg(int x, int y) {
     return(512);
   }
 
-  if ( x < 0 ) {
+  if (x < 0) {
     tx = -x;
-    if ( y < 0 ) {
+    if (y < 0) {
       ty = -y;
-      if ( tx > ty ) {
+      if (tx > ty) {
         f = 1;
-        od = DIV*3/4; tn = ty*TAN_TABLE_SIZE/tx;
+        od = DIV * 3 / 4;
+        tn = ty * TAN_TABLE_SIZE / tx;
       } else {
         f = -1;
-        od = DIV; tn = tx*TAN_TABLE_SIZE/ty;
+        od = DIV;
+        tn = tx * TAN_TABLE_SIZE / ty;
       }
     } else {
       ty = y;
-      if ( tx > ty ) {
+      if (tx > ty) {
         f = -1;
-        od = DIV*3/4; tn=ty*TAN_TABLE_SIZE/tx;
+        od = DIV * 3 / 4;
+        tn=ty * TAN_TABLE_SIZE/tx;
       } else {
-        f=1;
-        od = DIV/2; tn=tx*TAN_TABLE_SIZE/ty;
+        f = 1;
+        od = DIV / 2;
+        tn=tx * TAN_TABLE_SIZE / ty;
       }
     }
   } else {
     tx = x;
-    if ( y < 0 ) {
+    if (y < 0) {
       ty = -y;
-      if ( tx > ty ) {
+      if (tx > ty) {
         f = -1;
-        od = DIV/4; tn = ty*TAN_TABLE_SIZE/tx;
+        od = DIV / 4;
+        tn = ty * TAN_TABLE_SIZE / tx;
       } else {
         f = 1;
-        od = 0; tn = tx*TAN_TABLE_SIZE/ty;
+        od = 0;
+        tn = tx*TAN_TABLE_SIZE/ty;
       }
     } else {
       ty = y;
       if ( tx > ty ) {
         f = 1;
-        od = DIV/4; tn = ty*TAN_TABLE_SIZE/tx;
+        od = DIV/4;
+        tn = ty*TAN_TABLE_SIZE/tx;
       } else {
         f = -1;
-        od = DIV/2; tn = tx*TAN_TABLE_SIZE/ty;
+        od = DIV/2;
+        tn = tx*TAN_TABLE_SIZE/ty;
       }
     }
   }
-  return((od+tantbl[tn]*f)&(DIV-1));
+  return((od + tantbl[tn] * f)&(DIV-1));
 }
 
 int getDistance(int x, int y) {
