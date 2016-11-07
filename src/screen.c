5a0dd6ff4609b0f419a016a92360e8df8832046e
Mathieu Comandon <strycore@gmail.com>
2016-11-06 22:58:57 -0800

    Change type of keys to const Uint8

--- src/screen.c
+++ src/screen.c
@@ -110,7 +110,7 @@ static GLuint titleTexture;
 int lowres = 0;
 int windowMode = 0;
 int brightness = DEFAULT_BRIGHTNESS;
-Uint8 *keys;
+const Uint8 *keys;
 SDL_Joystick *stick = NULL;
 int joystickMode = 1;
 
